3bd772e9aeda3ab1e1e3c375df90613a6f34c188
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-10-23 17:15:01 +0200

    chatbot

---
+++ call_llm.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables
+
+myapikey = os.getenv('GEMINIKEY')
+
+
+from google import genai
+
+client = genai.Client(api_key=myapikey)
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+# )
+# print(response.text)
+
+def call_llm(prompt: str,modelinput: str = "gemini-2.5-flash") -> str:
+    response = client.models.generate_content(
+    model= modelinput, contents=prompt
+    )
+    return response.text
+print(call_llm("what's up"))
---
+++ mirror.py
@@ -0,0 +1,35 @@
+import streamlit as st
+print("start")
+st.title("Echo Echo")
+# Initialize chat history
+if "messages" not in st.session_state:
+    print("messages not found")
+    
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    print("messsage found:",message)
+    with st.chat_message(message["role"]):
+        print("write message to UI")
+        st.markdown(message["content"])
+        
+    
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    print("get user input")
+    
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        print("display user input")
+        
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+print("end")
