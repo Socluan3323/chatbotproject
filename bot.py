6948f4ccbd6a2b713feabe9200f757c5b9bb205b
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-11-01 20:14:57 +0700

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from prompt import bot_prompt
 
 class bot:
-    def __init__(self):
-        self.prompt = bot_prompt
-                
+    def __init__(self, input_Role, is_judge):
+        self.role = input_Role 
+        self.prompt = 
         
+    def build_prompt():
+            
     def respond(input):
         return None
     
