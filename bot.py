24fbfddd65810463caa48e1bfbbb9b888884278c
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-11-01 14:19:18 +0100

    a

--- bot.py
+++ bot.py
@@ -1,32 +1,13 @@
-from prompt import bot_prompt
+from prompt import bot_prompt, judge_prompt
 
 class bot:
     def __init__(self, input_Role, is_judge):
         self.role = input_Role 
-        self.prompt = 
+        self.prompt = judge_prompt if is_judge else bot_prompt
         
     def build_prompt():
             
     def respond(input):
         return None
     
-class agreeBot(bot):
-       def __init__(self,  ):
-        # add more attibute
-        
-        
-    def respond(input):
-        # write your code 
-        return response
-    
-    
-class disagreeBot(bot):
-       def __init__(self):
-         # add more attibute
-        
-        
-    def respond(input):
-        
-        # write your code 
-        return response
     
