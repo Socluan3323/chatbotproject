51b354de8942f2af27b85cb54704b95d2ef2785e
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-11-01 15:42:32 +0100

    a

--- bot.py
+++ bot.py
@@ -5,6 +5,13 @@ class bot:
     def __init__(self, conversation_history, topic):
         self.conversation_history = conversation_history
         self.topic = topic
+
+    def respond(self, new_conversation_history):
+        """pass in new conversation history to update the conversation history"""
+        self.conversation_history = new_conversation_history
+        response = call_llm(self.prompt)
+        return response
+    
         
     # def build_prompt(self,topic, history):
     #     self.prompt = self.prompt.format(role = self.role, topic = topic, history = history )
@@ -14,11 +21,18 @@ class bot:
     #     return call_llm(self.prompt)
     
 class AgreeBot(bot):
-    def __init__(self, conversation_history,topic):
-        super.__init__(conversation_history,topic)
-        self.prompt = bot_prompt.format(role = Role.agree, opposite_role = Role.disagree, conversation_history = self.conversation_history, topic = self.topic)
+    def __init__(self, conversation_history:str, topic: str):
+        super().__init__(conversation_history,topic)
+        self.prompt = bot_prompt.format(role = Role.agree.value, opposite_role = Role.disagree.value, conversation_history = self.conversation_history, topic = self.topic)
+    
 
+class DisagreeBot(bot):
+    def __init__(self,conversation_history: str, topic: str):
+        super().__init__(conversation_history,topic)
+        self.prompt = bot_prompt.format(role = Role.disagree.value, opposite_role = Role.agree.value, conversation_history = self.conversation_history, topic = self.topic )
 
 if __name__ == "__main__":
-    a = AgreeBot("","working at home is better than working at the office")
-    print(a.prompt)
+    # a = AgreeBot("","working at home is better than working at the office")
+    # print(a.respond())
+    b = DisagreeBot("","working at home is better than working at the office")
+    print(b.respond())
