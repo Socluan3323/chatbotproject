9cd59c66aa3f0ff67758bb8aec697d99cb0ae4c7
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-11-01 20:09:00 +0700

    add

---
+++ bot.py
@@ -0,0 +1,30 @@
+from prompt import bot_prompt
+
+class bot:
+    def __init__(self):
+        self.prompt = bot_prompt
+                
+        
+    def respond(input):
+        return None
+    
+class agreeBot(bot):
+       def __init__(self,  ):
+        # add more attibute
+        
+        
+    def respond(input):
+        # write your code 
+        return response
+    
+    
+class disagreeBot(bot):
+       def __init__(self):
+         # add more attibute
+        
+        
+    def respond(input):
+        
+        # write your code 
+        return response
+    
